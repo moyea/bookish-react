22cd421f73fcd49a16418e8a3b06a16c1fd79c5f
Qinghua <qinghua1994@hotmail.com>
2018-09-17 15:40:33 +0800

    Book List 重构

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+
+  renderBooks(books) {
+    return (<div className="books">
+      {
+        books.map(book => {
+          return (
+            <div className="book">
+              <h2 className="title">{book.name}</h2>
+            </div>
+          )
+        })
+      }
+    </div>)
+  }
+
   render() {
+    const books = [
+      {name: 'Refactoring'},
+      {name: 'Domain-driven design'}
+    ];
     return (
       <div className="App">
         <h1>Bookish</h1>
-        <div className="books">
-          <div className="book">
-            <h1 className="title">Refactoring</h1>
-          </div>
-          <div className="book">
-            <h1 className="title">Domain-driven design</h1>
-          </div>
-        </div>
+        {this.renderBooks(books)}
       </div>
     );
   }
