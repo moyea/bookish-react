c75c2ad832e3d83160588df308543b2ec0067919
Qinghua <qinghua1994@hotmail.com>
2018-09-18 14:55:35 +0800

    refactor: 移除BookListContainer中重复的代码

--- src/containers/BookListContainer.js
+++ src/containers/BookListContainer.js
@@ -6,42 +6,39 @@ class BookListContainer extends Component {
   state = {
     books: [],
     loading: true,
-    error: null
+    error: null,
+    term: ''
   };
 
   componentDidMount() {
-    axios.get('http://localhost:8080/books')
-      .then(res => {
-        this.setState({
-          books: res.data,
-          loading: false
-        });
-      })
-      .catch(err => {
-        this.setState({
-          loading: false,
-          error: err
-        });
-      });
+    this.fetchBooks();
+  }
+
+  updateBooks(res) {
+    this.setState({
+      books: res.data,
+      loading: false
+    });
+  }
+
+  updateError(err) {
+    this.setState({
+      loading: false,
+      error: err
+    });
   }
 
   filterBook(e) {
     this.setState({
       term: e.target.value
-    });
-    axios.get(`http://localhost:8080/books?q=${e.target.value}`)
-      .then(res => {
-        this.setState({
-          books: res.data,
-          loading: false
-        });
-      })
-      .catch(err => {
-        this.setState({
-          loading: false,
-          error: err
-        })
-      });
+    }, this.fetchBooks);
+  }
+
+  fetchBooks() {
+    const {term} = this.state;
+    axios.get(`http://localhost:8080/books?q=${term}`)
+      .then(res => this.updateBooks(res))
+      .catch(err => this.updateError(err));
   }
 
   render() {
